87c182ce54fff7c778d0e49a76429af53ce52e58
OthmaneAGHBAL <89691001+OthmaneAGHBAL@users.noreply.github.com>
2023-01-08 22:58:45 +0100

    Add files via upload

---
+++ src/components/Citation/Citation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container} from 'reactstrap';
+
+
+const Citation = () => {
+    return (
+          <section className=" d-flex px-5 align-content-center text-center justify-content-center fs-4 text-light py-3 bg-dark">
+               ={'>'} 
+               <div className=" border-4 mx-2 border-light border px-4">
+                    Think Outside The
+               </div>
+               <div className=" mx-2">Box</div>
+                {'<'}=
+          </section>
+    )
+}
+
+export default Citation;
